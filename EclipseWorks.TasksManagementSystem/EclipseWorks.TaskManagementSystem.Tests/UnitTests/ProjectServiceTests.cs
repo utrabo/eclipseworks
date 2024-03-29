bee5ff2a9acfbe3b7a56e9b3093781881b83ccb9
utrabo <utrabo@gmail.com>
2024-03-29 17:55:17 -0300

    validating the exception message on test project deletion when there is pending tasks

--- EclipseWorks.TasksManagementSystem/EclipseWorks.TaskManagementSystem.Tests/UnitTests/ProjectServiceTests.cs
+++ EclipseWorks.TasksManagementSystem/EclipseWorks.TaskManagementSystem.Tests/UnitTests/ProjectServiceTests.cs
@@ -13,7 +13,7 @@ namespace EclipseWorks.TaskManagementSystem.Tests.UnitTests;
 public class ProjectServiceTests
 {
     [Fact]
-    public async void DeleteProjectAsync_WithPendingTasks_ThrowsException()
+    public async Task DeleteProjectAsync_WithPendingTasks_ThrowsException()
     {
         // Arrange
         var projectId = 1;
@@ -21,12 +21,16 @@ public class ProjectServiceTests
         mockProjectRepository.Setup(repo => repo.GetTasksByProjectIdAsync(projectId))
                              .ReturnsAsync(new List<ProjectTask>
                              {
-                                new ProjectTask(ProjectTaskPriority.Low) { Title = "Task 1", Description = "Task 1", DueDate = DateTime.Now, Status = ProjectTaskStatus.Pending, ProjectId = 1 }
+                            new ProjectTask(ProjectTaskPriority.Low) { Title = "Task 1", Description = "Task 1", DueDate = DateTime.Now, Status = ProjectTaskStatus.Pending, ProjectId = projectId }
                              });
 
-        IProjectService service = new ProjectService(mockProjectRepository.Object);
+        var service = new ProjectService(mockProjectRepository.Object);
 
-        // Act & Assert
-        await Assert.ThrowsAsync<InvalidOperationException>(() => service.DeleteProjectAsync(projectId));
+        // Act
+        var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => service.DeleteProjectAsync(projectId));
+
+        // Assert
+        Assert.Equal("Project has pending tasks. Complete or remove the tasks first.", exception.Message);
     }
+
 }
